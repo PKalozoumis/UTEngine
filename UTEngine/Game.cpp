0018d58ed94b1dfef7b59e45af993d1d23e90116
UnavailableUsernameException <51862240+UnavailableUsernameException@users.noreply.github.com>
2021-07-04 12:30:43 +0300

    Add project files.

---
+++ UTEngine/Game.cpp
@@ -0,0 +1,35 @@
+#include "Game.h"
+#include "SDL.h"
+
+Game::Game(){}
+Game::~Game(){}
+
+void Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+
+}
+
+void Game::handleEvents()
+{
+
+}
+
+void Game::update()
+{
+
+}
+
+void Game::render()
+{
+
+}
+
+void Game::clean()
+{
+
+}
+
+inline bool Game::running()
+{
+	return isRunning;
+}
